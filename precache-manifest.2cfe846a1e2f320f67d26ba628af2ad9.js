a67be1fa5e33f948197e591d63ac66cac3d9a8b7
saahilshaikh <44947415+saahilshaikh@users.noreply.github.com>
2019-04-05 14:15:27 +0530

    Updates

---
+++ precache-manifest.2cfe846a1e2f320f67d26ba628af2ad9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e598cc6c1442c396b59",
+    "url": "/countdown/static/js/runtime~main.6f2a1a9a.js"
+  },
+  {
+    "revision": "c3041e6c159e9121d92f",
+    "url": "/countdown/static/js/main.52ec1edd.chunk.js"
+  },
+  {
+    "revision": "c9a9f258b11d0ac64772",
+    "url": "/countdown/static/js/2.4526bd2f.chunk.js"
+  },
+  {
+    "revision": "c3041e6c159e9121d92f",
+    "url": "/countdown/static/css/main.984eb21a.chunk.css"
+  },
+  {
+    "revision": "112a2065253be3fcb1409c9e1b4ab053",
+    "url": "/countdown/index.html"
+  }
+];
